3c29bfd63622f4c295d1f5cf1dc6f29fea4c8fe9
holutunde <holutunde99@gmail.com>
2021-09-09 16:55:12 +0100

    first commit

--- app/src/main/java/com/example/dialogfragmant/DialogFragment.kt
+++ app/src/main/java/com/example/dialogfragmant/DialogFragment.kt
@@ -1,4 +1,20 @@
 package com.example.dialogfragmant
 
-class DialogFragment {
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import android.view.View
+import android.view.ViewGroup
+import android.view.LayoutInflater
+
+
+class DialogFragment: DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+     ): View?{
+        var root: View = inflater.inflate(R.layout.fragment_dialog, container, false)
+
+        return root
+    }
 }
--- app/src/main/java/com/example/dialogfragmant/MainActivity.kt
+++ app/src/main/java/com/example/dialogfragmant/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.dialogfragmant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rateTextView: TextView = findViewById(R.id.rateTextView)
+        rateTextView.setOnClickListener{
+            var dialog = DialogFragment()
+
+            dialog.show(supportFragmentManager, "customDialog")
+        }
+
     }
 }
